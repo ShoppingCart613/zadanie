00b01d2c0ea5f0b6304c2b5bfbcc19b477c3a409
student <e@e>
2023-12-14 14:14:37 +0100

    initial

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from layout import Ui_Dialog
 
 class MyForm(QDialog):
         def __init__(self):
+            isValid = True
             super().__init__()
             self.ui = Ui_Dialog()
             self.ui.setupUi(self)
-            self.ui.zapisz.clicked.connect(self.walidacja)
+            self.ui.zapisz.clicked.connect(self.zapisz_do_listy)
             self.ui.zapiszplik.clicked.connect(self.zapisz_do_pliku)
             self.show()
 
@@ -22,16 +23,28 @@ class MyForm(QDialog):
             pesel = self.ui.pesel.text()
             imie = self.ui.imie.text()
             nazwisko = self.ui.nazwisko.text()
+            if(len(imie) < 3):
+                alert.setInformativeText("za krótkie imie (minimum 3)")
+                alert.exec()
+                self.isValid = False
+                return False
+            if (len(nazwisko) < 3):
+                alert.setInformativeText("za krótkie nazwisko (minimum 3)")
+                alert.exec()
+                self.isValid = False
+                return False
             if(len(numer) == 9):
                 try:
                     numer = int(numer)
                 except:
                     alert.setInformativeText("nieprawidłowy numer telefonu")
                     alert.exec()
+                    self.isValid = False
                     return False
             else:
                 alert.setInformativeText("za krótki numer telefonu")
                 alert.exec()
+                self.isValid = False
                 return False
             if (len(pesel) == 11):
                 try:
@@ -39,10 +52,12 @@ class MyForm(QDialog):
                 except:
                     alert.setInformativeText("nieprawidłowy PESEL")
                     alert.exec()
+                    self.isValid = False
                     return False
             else:
                 alert.setInformativeText("za krótki pesel")
                 alert.exec()
+                self.isValid = False
                 return False
 
             waga = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
@@ -51,24 +66,28 @@ class MyForm(QDialog):
             suma = int(pesel[0]) * waga[0] + int(pesel[1]) * waga[1] + int(pesel[2]) * waga[2] + int(pesel[3]) * waga[3] + int(pesel[4]) * waga[4] + int(pesel[5]) * waga[5] + int(pesel[6]) * waga[6] + int(pesel[7]) * waga[7] + int(pesel[8]) * waga[8] + int(pesel[9]) * waga[9]
             if suma >= 10:
                 suma = (suma%10)
-            if int(pesel[10])==10-suma%10:
-                alert.setWindowTitle(":)")
-                alert.setInformativeText("dodano")
-                alert.exec()
-                nazwa = imie +" "+ nazwisko
-                self.ui.lista.addItem(nazwa )
-            else:
+            if int(pesel[10])!=10-suma%10:
                 alert.setInformativeText("zły pesel")
                 alert.exec()
+                self.isValid = False
                 return False
+
+        def zapisz_do_listy(self):
+            self.walidacja()
+            if self.isValid == True:
+                imie = self.ui.imie.text()
+                nazwisko = self.ui.nazwisko.text()
+                nazwa = imie + " " + nazwisko
+                self.ui.lista.addItem(nazwa)
 
         def zapisz_do_pliku(self):
             self.walidacja()
-            zapis=""
-            for i in range(self.ui.lista.count()):
-                zapis += self.ui.lista.itemText(i)+"\n"
-                with open("lista.txt","w") as file:
-                    file.write(zapis)
+            if self.isValid == True:
+                imie = self.ui.imie.text()
+                nazwisko = self.ui.nazwisko.text()
+                nazwa = imie + " " + nazwisko + "\n"
+                with open("lista.txt","a") as file:
+                    file.write(nazwa)
 
 
 
